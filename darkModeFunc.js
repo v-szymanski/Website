f4817503e4a4384854d7d313cff3808c22e62633
v-szymanski <v2szyman@uwaterloo.ca>
2021-08-31 17:19:58 -0400

    Landing page

--- darkModeFunc.js
+++ darkModeFunc.js
@@ -1,4 +1,22 @@
+var ifDark = false;
+
 function darkModeFunc() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    const lightBackground = document.querySelector('.darkModeContainer');
+    const darkBackground = document.querySelector('.darkModeContainer2');
+
+    if (ifDark){
+        ifDark = false;
+        lightBackground.classList.remove('hidden');
+        var element = document.body;
+        element.classList.remove("dark-mode");
+    }
+    
+    else{
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+        lightBackground.classList.add('hidden');
+        ifDark = true;
+    }
+    
+
 }
